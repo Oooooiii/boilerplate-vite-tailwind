5b0ddc37c6bbe3afa7dfe1ab674c992ffdb3e0d6
Oooooiii <hearong.200@gmail.com>
2021-10-02 13:43:13 +0700

    Update: code

--- src/components/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1 class="mb-6 text-3xl text-red-600 font-extrabold">Somewhere in Home</h1>
-</template>
-
-<script setup></script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './index.css'
+import './assets/tailwind.css'
 
-createApp(App).use(router).use(store).mount('#app')
+let app
+
+app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.mount('#app')
